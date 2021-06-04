79b5b15fa8ab91cb388d767f652198f5c40c7fa1
dura51 <83475148+dura51@users.noreply.github.com>
2021-06-04 07:09:36 -0400

    Add files via upload

---
+++ who.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Hi this is my tool to show you who is connect with your operating system, who is you and what are the process in your operating system."
+who
+echo "--------------------"
+whoami
+echo "--------------------"
+w
